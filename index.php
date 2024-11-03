f6ec1e64a48aa7447a9ad153a6b960349c7bff92
SergiumarK <s.marcocimd@gmail.com>
2024-11-03 23:02:33 +0200

    PDO_Firt_Step

--- index.php
+++ index.php
@@ -3,4 +3,19 @@
 
 require 'functions.php';
 
-require 'router.php';
+// require 'router.php';
+
+$dsn = "mysql:host=localhost;port=3306;user=root;dbname=cara;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$stmt = $pdo ->prepare("select * from posts");
+$stmt->execute();
+
+$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
