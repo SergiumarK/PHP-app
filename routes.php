ebd5c3287b4ed3dd766be2bc5a1fa025bfe4aea9
SergiumarK <s.marcocimd@gmail.com>
2024-11-19 17:26:34 +0200

    Extract_a_Form_Validation_Object

--- routes.php
+++ routes.php
@@ -24,29 +24,29 @@
 
 
 
-$router->get('/Cara/', 'controllers/main.php');
-$router->get('/Cara/about', 'controllers/about.php');
-$router->get('/Cara/blog', 'controllers/blog.php',);
-$router->get('/Cara/cart', 'controllers/cart.php');
-$router->get('/Cara/contact', 'controllers/contact.php');
+$router->get('/Cara/', 'main.php');
+$router->get('/Cara/about', 'about.php');
+$router->get('/Cara/blog', 'blog.php',);
+$router->get('/Cara/cart', 'cart.php');
+$router->get('/Cara/contact', 'contact.php');
 
-$router->get('/Cara/notes', 'controllers/notes/index.php')->only('auth');
-$router->get('/Cara/note', 'controllers/notes/show.php');
-$router->delete('/Cara/note', 'controllers/notes/destroy.php');
+$router->get('/Cara/notes', 'notes/index.php')->only('auth');
+$router->get('/Cara/note', 'notes/show.php');
+$router->delete('/Cara/note', 'notes/destroy.php');
 
-$router->get('/Cara/note/edit', 'controllers/notes/edit.php');
-$router->patch('/Cara/note', 'controllers/notes/update.php');
+$router->get('/Cara/note/edit', 'notes/edit.php');
+$router->patch('/Cara/note', 'notes/update.php');
 
-$router->get('/Cara/notes/create', 'controllers/notes/create.php');
-$router->post('/Cara/notes', 'controllers/notes/store.php');
+$router->get('/Cara/notes/create', 'notes/create.php');
+$router->post('/Cara/notes', 'notes/store.php');
 
-$router->get('/Cara/products', 'controllers/prod/index.php');
-$router->get('/Cara/product', 'controllers/prod/show.php');
-$router->get('/Cara/products/create', 'controllers/prod/create.php');
+$router->get('/Cara/products', 'prod/index.php');
+$router->get('/Cara/product', 'prod/show.php');
+$router->get('/Cara/products/create', 'prod/create.php');
 
-$router->get('/Cara/register', 'controllers/registration/create.php')->only('guest');
-$router->post('/Cara/register', 'controllers/registration/store.php')->only('guest');
+$router->get('/Cara/register', 'registration/create.php')->only('guest');
+$router->post('/Cara/register', 'registration/store.php')->only('guest');
 
-$router->get('/Cara/login', 'controllers/session/create.php')->only('guest');
-$router->post('/Cara/session', 'controllers/session/store.php')->only('guest');
-$router->delete('/Cara/session', 'controllers/session/destroy.php')->only('auth');
+$router->get('/Cara/login', 'session/create.php')->only('guest');
+$router->post('/Cara/session', 'session/store.php')->only('guest');
+$router->delete('/Cara/session', 'session/destroy.php')->only('auth');
